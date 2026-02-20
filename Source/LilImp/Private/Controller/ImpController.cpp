067e8f429012f6e2f20e33bc5b15eca628ab1b2a
eljassar <jassar.art@gmail.com>
2026-02-20 06:45:50 +0300

    Add enhanced input bindings to ImpController
    Integrates Unreal Enhanced Input into the ImpController: adds includes, constructor, BeginPlay, and SetupInputComponent to bind Flap and Fire actions via UEnhancedInputComponent. Exposes UInputMappingContext, FlapAction and FireAction as editable UPROPERTYs and adds placeholder handlers for action events. Also updates .gitignore to ignore *.xml files. Handlers are currently basic/stubbed and may need further implementation or type fixes.

--- Source/LilImp/Private/Controller/ImpController.cpp
+++ Source/LilImp/Private/Controller/ImpController.cpp
@@ -1,5 +1,50 @@
 // Jassar alotaibi | Weaver of worlds | all rights reserved.
-
-
 #include "Controller/ImpController.h"
+#include "InputMappingContext.h"
+#include "InputAction.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
 
+// Sets default values
+AImpController::AImpController()
+{
+	
+}
+
+// Called when the game starts or when spawned
+void AImpController::BeginPlay()
+{
+
+}
+
+// Called to bind functionality to input
+void AImpController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+    
+	if (UEnhancedInputComponent* EnhancedInput = Cast<UEnhancedInputComponent>(InputComponent))
+	{
+		if (FlapAction)
+		{
+			EnhancedInput->BindAction(FlapAction, ETriggerEvent::Started, this, &AImpController::HandleFlapAction);
+			EnhancedInput->BindAction(FlapAction, ETriggerEvent::Completed, this, &AImpController::HandleFlapAction);
+		}
+        
+		if (FireAction)
+		{
+			EnhancedInput->BindAction(FireAction, ETriggerEvent::Triggered, this, &AImpController::HandleFireAction);
+		}
+	}
+}
+
+// Handle Flap action input
+void AImpController::HandleFlapAction(const FInputActionValue& Value)
+{
+	FlapAction = Value.Get<bool>();
+}
+
+// Handle Fire action input
+void AImpController::HandleFireAction(const FInputActionValue& Value)
+{
+	
+}
--- Source/LilImp/Public/Controller/ImpController.h
+++ Source/LilImp/Public/Controller/ImpController.h
@@ -1,17 +1,46 @@
 // Jassar alotaibi | Weaver of worlds | all rights reserved.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+#include "InputMappingContext.h"
+#include "InputAction.h"
 #include "ImpController.generated.h"
 
 /**
- * 
+ *  will set the input action, mapping context.
+ *  with possible ot other scripts so use the input here to play there functions
+ *  for  Imp, only two inputs The Flap and Fire suing both Space and Shift buttons
  */
+class 	UInputMappingContext;
+class   UInputAction;
+
+
 UCLASS()
 class LILIMP_API AImpController : public APlayerController
 {
 	GENERATED_BODY()
+public:
+	AImpController();
+	
+	// Define the input action and mapping context for the Imp character
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputMappingContext* DefaultImpIMC;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputAction* FlapAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputAction* FireAction;
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+	
+private:
+	
+	// Called to bind functionality to input
+	void HandleFlapAction(const FInputActionValue& Value);
+	void HandleFireAction(const FInputActionValue& Value);
 	
 };
